eaacaabb93929cc9208230e9f14bcd688e16c205
CharlieRonchetti <85499184+CharlieRonchetti@users.noreply.github.com>
2024-10-11 15:46:42 +0100

    Add basic GUI home screen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using OsuReplayAnalyser.ViewModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,7 @@ namespace OsuReplayAnalyser
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using OsuReplayAnalyser.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuReplayAnalyser.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+		private ViewModelBase? selectedViewModel;
+
+		public ViewModelBase? SelectedViewModel
+        {
+			get { return selectedViewModel; }
+			set { selectedViewModel = value; }
+		}
+
+	}
+}
