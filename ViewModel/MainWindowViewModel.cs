3bc9fe701e77a9db63a9a795c836c0e682a85290
CharlieRonchetti <85499184+CharlieRonchetti@users.noreply.github.com>
2024-10-14 18:24:12 +0100

    Add basic navigation

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,21 +1,69 @@
 ﻿using OsuReplayAnalyser.MVVM;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace OsuReplayAnalyser.ViewModel
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-		private ViewModelBase? selectedViewModel;
+        public RelayCommand UpdateViewCommand => new RelayCommand(execute => UpdateView(execute), canExecute => CanUpdateView(canExecute));
+
+        private ViewModelBase? selectedViewModel = new LandingPageViewModel();
 
 		public ViewModelBase? SelectedViewModel
         {
 			get { return selectedViewModel; }
-			set { selectedViewModel = value; }
+			set { 
+                selectedViewModel = value;
+                OnPropertyChanged();
+            }
 		}
 
-	}
+        private Visibility landingButtonVisibility = Visibility.Collapsed;
+
+        public Visibility LandingButtonVisibility
+        {
+            get { return landingButtonVisibility; }
+            set {
+                landingButtonVisibility = value; 
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string? CurrentView = "Landing";
+
+        public void UpdateView(object view)
+        {
+            Debug.WriteLine(view.ToString());
+            if (view.ToString() == "HoldTimes")
+            {
+                this.SelectedViewModel = new HoldTimesViewModel();
+                this.LandingButtonVisibility = Visibility.Visible; 
+                this.CurrentView = view.ToString();
+            }
+            else if (view.ToString() == "Landing")
+            {
+                this.SelectedViewModel = new LandingPageViewModel();
+                this.LandingButtonVisibility = Visibility.Collapsed;
+                this.CurrentView = view.ToString();
+            }
+        }
+
+        public bool CanUpdateView(object view)
+        {
+            if(view.ToString() != this.CurrentView)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
 }
